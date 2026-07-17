03ed9dd6855c0bc724ff7edbd8b6f6326184b438
Jerssao <ojefferson.af@gmail.com>
2026-07-16 21:51:12 -0300

    Estilizando menus

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 $uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($uri, PHP_URL_PATH);
 
 $routes = [
     '/' => 'home',
--- resources/views/include/top-nav.php
+++ resources/views/include/top-nav.php
@@ -3,14 +3,13 @@
 
                 <div class="flex items-center justify-start gap-4 h-16">
                     <a href="/"
-                        class="text-white py-2 px-3 leading-lg rounded-md hover:bg-gray-900 transition transition-all duration[0.5]"> 
+                        class=<?=  route_is('/') ? 'bg-gray-900 text-white' : 'text-gray-400' ?> py-2 px-3 leading-lg rounded-md hover:bg-gray-900 transition transition-all duration[0.5]">
                         Home
                     </a>
                     <a href="/contact"
-                        class="text-white py-2 px-3 leading-lg rounded-md hover:bg-gray-900 transition transition-all duration[0.5]">
+                        class=<?=  route_is('/contact') ? 'bg-gray-900 text-white' : 'text-gray-400' ?>  hover:bg-gray-900 py-2 px-3 leading-lg rounded-md transition transition-all duration[0.5]">
                         Contact
                     </a>
                 </div>
-
             </div>
         </nav>
